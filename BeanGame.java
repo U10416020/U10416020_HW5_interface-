b32e353980711438cb95c1b4724f9027b8b7eec5
U10416020 <wkps19961015@gmail.com>
2016-03-17 19:34:23 +0800

    Create some circle
    Create some circle for the bean machine.

--- BeanGame.java
+++ BeanGame.java
@@ -69,6 +69,71 @@ public class BeanGame extends Application{
 			//Add all line to the pane
 			getChildren().addAll(lineH, lineV1, lineV2, lineV3, lineV4, lineV5, 
 			lineV6, lineV7, lineV8, lineV9, lineS1, lineS2, lineV10, lineV11);
+			
+			//Create and set seven circle
+			Circle circle7_1 = new Circle(130, 310, 10);
+			Circle circle7_2 = new Circle(170, 310, 10);
+			Circle circle7_3 = new Circle(210, 310, 10);
+			Circle circle7_4 = new Circle(250, 310, 10);
+			Circle circle7_5 = new Circle(290, 310, 10);
+			Circle circle7_6 = new Circle(330, 310, 10);
+			Circle circle7_7 = new Circle(370, 310, 10);
+		
+			//Add seven circle to the pane
+			getChildren().addAll(circle7_1, circle7_2, circle7_3, circle7_4, circle7_5,
+			circle7_6, circle7_7);
+		
+			//Create and set six circle
+			Circle circle6_1 = new Circle(150, 280, 10);
+			Circle circle6_2 = new Circle(190, 280, 10);
+			Circle circle6_3 = new Circle(230, 280, 10);
+			Circle circle6_4 = new Circle(270, 280, 10);
+			Circle circle6_5 = new Circle(310, 280, 10);
+			Circle circle6_6 = new Circle(350, 280, 10);		
+		
+			//Add six circle to the pane
+			getChildren().addAll(circle6_1, circle6_2, circle6_3, circle6_4, circle6_5,
+			circle6_6);		
+		
+			//Create and set five circle
+			Circle circle5_1 = new Circle(170, 250, 10);
+			Circle circle5_2 = new Circle(210, 250, 10);
+			Circle circle5_3 = new Circle(250, 250, 10);
+			Circle circle5_4 = new Circle(290, 250, 10);				
+			Circle circle5_5 = new Circle(330, 250, 10);
+		
+			//Add five circle to the pane
+			getChildren().addAll(circle5_1, circle5_2, circle5_3, circle5_4, circle5_5);
+		
+			//Create and set four circle
+			Circle circle4_1 = new Circle(190, 220, 10);
+			Circle circle4_2 = new Circle(230, 220, 10);
+			Circle circle4_3 = new Circle(270, 220, 10);
+			Circle circle4_4 = new Circle(310, 220, 10);			
+		
+			//Add four circle to the pane
+			getChildren().addAll(circle4_1, circle4_2, circle4_3, circle4_4);
+		
+			//Create and set three circle
+			Circle circle3_1 = new Circle(210, 190, 10);
+			Circle circle3_2 = new Circle(250, 190, 10);
+			Circle circle3_3 = new Circle(290, 190, 10);	
+		
+			//Add three circle to the pane
+			getChildren().addAll(circle3_1, circle3_2, circle3_3);
+		
+			//Create and set two circle
+			Circle circle2_1 = new Circle(230, 160, 10);
+			Circle circle2_2 = new Circle(270, 160, 10);				
+		
+			//Add two circle to the pane
+			getChildren().addAll(circle2_1, circle2_2);
+		
+			//Create and set one circle		
+			Circle circle1 = new Circle(250, 130, 10);
+		
+			//Add the circle to the pane
+			getChildren().add(circle1);	
 		}
 	}		
 }
