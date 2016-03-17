1dcb2640de74d34dc1366e5f0539543ca71ea021
U10416020 <wkps19961015@gmail.com>
2016-03-17 19:35:31 +0800

    Create a class named TestBeanGame
    Set the main method to display the bean machine.

---
+++ TestBeanGame.java
@@ -0,0 +1,11 @@
+//U10416020
+
+import javafx.application.Application;
+
+public class TestBeanGame{
+	//Main method
+	public static void main(String[] args){	
+		//Display the BeanGame
+		Application.launch(BeanGame.class, args);
+	}
+}
